48473708a8e3c1396a771004027916c2c3103b9a
Steven Blair <steven.blair@synapt.ec>
2021-05-25 23:06:53 +0100

    updated WS and monitoring

--- streamprotocol.go
+++ streamprotocol.go
@@ -20,7 +20,7 @@ const Simple8bThresholdSamples = 16
 
 // TODO value of 5 is good in streamprotocol_test.go, but slightly worse for microcontroller data (0.36 Mbps vs 0.3 Mbps)
 // DefaultDeltaEncodingLayers defines the default number of layers of delta encoding. 0 is no delta encoding (just use varint), 1 is delta encoding, etc.
-const DefaultDeltaEncodingLayers = 5
+const DefaultDeltaEncodingLayers = 3
 
 // HighDeltaEncodingLayers defines the number of layers of delta encoding for high sampling rate scenarios.
 const HighDeltaEncodingLayers = 3
